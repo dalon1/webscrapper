9318d6d8180055aca57372e94244998581d6547b
dalon1 <dannelvision@gmail.com>
2018-04-26 22:11:16 -0500

    Creating csv files from yaml data

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
+# To install module, please enter command in terminal:
+# pip install pyyaml
 import yaml
+
+import csv
 
 # The next function reads data from a yaml file
 # and displays it into the console
@@ -12,11 +16,22 @@ def builDictionary(stream):
 def readYaml(file_name):
     with open(file_name, "r") as stream:
         try:
-            builDictionary(yaml.load(stream)['army'])
+            data = yaml.load(stream)['army']
+            builDictionary(data)
+            createCSV('csvtest1.csv', data)
             #print(yaml.load(stream)['army'])
         except yaml.YAMLError as exc:
             print("Error: " + exc)
 
- 
+def createCSV(file_name, stream):
+    # removing that extra blank line created by default between records.
+    with open(file_name, 'w', newline='') as csvfile:
+        # Creating file writer (python) or stream writer (c#)
+        file_writer = csv.writer(csvfile, delimiter = ',', 
+        dialect = 'excel', quoting = csv.QUOTE_MINIMAL, quotechar='|')
+        for key, value in stream.items():
+            file_writer.writerow([key, value])
+
+
 
 readYaml(FILE_NAME)
